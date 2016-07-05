74235b72fa7174c982873aa69620b44e6228c4f9
ysj16 <504427021@qq.com>
2016-07-05 20:35:35 +0800

    fixed bug

--- app/controllers/article.js
+++ app/controllers/article.js
@@ -16,7 +16,8 @@ exports.list = function(req,res,next){
 	}
 	else{
 		var Article = require("../models/article");
-	    var allDates = Article.find(query).populate("tags").sort({isTop:-1}).sort({"meta.createAt":1});
+	    var allDates = Article.find(query).populate("tags").sort({isTop:-1}).sort({"meta.createAt":-1});
+	    console.log(2)
 	    allDates
 	      .exec(function(err){if(err) console.log(err)})
 	      .then(function(data){
